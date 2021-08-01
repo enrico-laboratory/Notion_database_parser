7a06517b0cff94a7f7bff8546dbf2a343f8c1581
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2021-08-01 13:54:53 +0200

    added  GetRecords Class, optimized ReturnDbDict

--- notion_extract_db_record.py
+++ notion_extract_db_record.py
@@ -1,13 +1,39 @@
-from app.pull_db import ReturnDbDict
+from app.pull_db import ReturnDbDict, GetRecords
 import config
 import pprint
 
-project_db = ReturnDbDict(config.PROJECT_DB_ID, 'project_db')
-location_db = ReturnDbDict(config.LOCATION_DB_ID, 'location_db')
-cast_db = ReturnDbDict(config.CAST_DB_ID, 'cast_db')
-repertoire_db = ReturnDbDict(config.REPERTOIRE_DB_ID, 'repertoire_db')
 
-pprint.pprint(project_db.dump_database())
-pprint.pprint(location_db.dump_database())
-pprint.pprint(cast_db.dump_database())
-pprint.pprint(repertoire_db.dump_database())
+project_db = ReturnDbDict(config.PROJECT_DB_ID, 'project_db').read_database()
+location_db = ReturnDbDict(config.LOCATION_DB_ID, 'location_db').read_database()
+cast_db = ReturnDbDict(config.CAST_DB_ID, 'cast_db').read_database()
+repertoire_db = ReturnDbDict(config.REPERTOIRE_DB_ID, 'repertoire_db').dump_database()
+
+# pprint.pprint(project_db)
+# pprint.pprint(location_db)
+# pprint.pprint(cast_db.dump_database())
+# pprint.pprint(repertoire_db.dump_database())
+
+project_db_records = GetRecords(project_db)
+repertoire_db_records = GetRecords(repertoire_db)
+cast_db_records = GetRecords(cast_db)
+location_db_records = GetRecords(location_db)
+
+# pprint.pprint(project_db_records.get_properties(0))
+# pprint.pprint(repertoire_db_records.get_properties(0))
+
+
+# for i in range(len(project_db)):
+#     pprint.pprint(project_db_records.get_record_title(i))
+
+# for i in range(len(cast_db)):
+#     print('====== NEW RECORD ======')
+#     pprint.pprint(cast_db_records.parse_database(i))
+
+# for i in range(len(repertoire_db)):
+#     print('====== NEW RECORD ======')
+#     pprint.pprint(repertoire_db_records.parse_database(i))
+
+for i in range(len(location_db)):
+    print('====== NEW RECORD ======')
+    pprint.pprint(location_db_records.get_record_id(i))
+    pprint.pprint(location_db_records.parse_database(i))
